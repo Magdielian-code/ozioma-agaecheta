b115a8f0d642b64ffd5fac7392ad98f009aed869
magdielian-code <oziomaagaecheta81@gmail.com>
2025-03-21 15:17:16 +0100

    solved descrepancy.

--- ozioma-agaecheta/next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    experimental: {
-        optimizePackageImports: ["@chakra-ui/react", "@chakra-ui/icons"],
-    },
-};
-
-export default nextConfig;
